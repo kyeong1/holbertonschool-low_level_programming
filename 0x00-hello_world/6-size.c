e2b553cb5256fe9cd2ee5660d4f4d3deee4ac668
Kevin Yeong <kyeong1@yahoo.com>
2019-06-20 17:06:37 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 printf("Size of a char: %d Byte(s)\n"), sizeof(char);
 printf("Size of an int: %d Byte(s)\n"), sizeof(int);
-printf("Size of a long int: %d Byte(s)\n"), sizeof(long int);
-printf("Size of a long long int: %d Byte(s)\n"), sizeof(long long int);
-printf("Size of a float: %d Byte(s)\n"), sizeof(float);
+printf("Size of a long int: %ld Byte(s)\n"), sizeof(long int);
+printf("Size of a long long int: %ld Byte(s)\n"), sizeof(long long int);
+printf("Size of a float: %g Byte(s)\n"), sizeof(float);
 return (0);
 }
