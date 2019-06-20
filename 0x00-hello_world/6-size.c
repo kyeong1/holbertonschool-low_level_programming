3a94b55b501b4f28e2084220cab661a2c754347b
Kevin Yeong <kyeong1@yahoo.com>
2019-06-20 05:38:26 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-
 /*
- * main - sizeof 
- *
- * Return: Always 0 (Success)
- */
+*main-sizeof 
+*Return:Always 0 (Success)
+*/
 int main(void)
 {
-char=c
-int=i
-long int=li
-long long int=Li
-float=f
-
+char = c
+int = i
+long int = li
+long long int = Li
+float = f
 printf("Size of a char: %ld byte(s)"\n), sizeof(c);
 printf("Size of an int: %ld byte(s)"\n), sizeof(i);
 printf("Size of a long int: %ld byte(s)"\n), sizeof(li);
 printf("Size of a long long int: %ld byte(s)"\n), sizeof(Li);
 printf("Size of a float: %ld byte(s)"\n), sizeof(f);
 return (0);
-}      
+}
