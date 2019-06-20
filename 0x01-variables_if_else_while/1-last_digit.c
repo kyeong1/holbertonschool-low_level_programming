d1c029e94f4fc58659e395c36d4d029252eb0c0c
Kevin Yeong <kyeong1@yahoo.com>
2019-06-20 21:14:11 +0000

    Random last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,17 +8,18 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 5)
+int y;
+y = n % 10;
 {
-printf("Last digit is %d and is greater than 5\n", n);
-}
-if (n == 0)
-{
-printf("Last digit is %d and is 0\n", n);
-}
-if (n < 6)
-{
-printf("Last digit is %d and is less than 6 and not 0\n", n);
-}
+printf("Last digit of %d is %d", n, y);
+if (y > 5)
+printf("and is greater than 5\n");
+printf("Last digit of %d is %d", n, y);
+if (y == 0)
+printf("and is 0\n");
+printf("Last digit of %d is %d", n, y);
+if (y < 6)
+printf("and is less than 6 and not 0");
 return (0);
+ }
 }
