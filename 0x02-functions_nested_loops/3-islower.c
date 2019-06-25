4f2a0d89a467401d710d7c16e61ac131a59b35ba
Kevin Yeong <kyeong1@yahoo.com>
2019-06-25 18:09:49 +0000

    Lower characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _islower - checks for lowercase characters
+ *
+ * Return: 1 if lowercase, 0 if uppercase
+ *
+ **/
+int _islower(int a);
+{
+if (a <= 'z' && a >= 'a')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
