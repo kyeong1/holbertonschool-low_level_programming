66116d8c7e1f7e52f36f1ec9aa713571e3f968b1
Kevin Yeong <kyeong1@yahoo.com>
2019-06-25 21:23:55 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _abs - function that prints out the absolute value
+ * @n: takes integer
+ * Return 1 if n > 0, 0 if n == 0, positive if negative
+ *
+ **/
+int _abs(int)
+int n;
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}
+else ('-' == '+')
+}
