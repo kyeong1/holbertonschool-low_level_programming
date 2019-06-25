155e329a3ceebb8ab802ec8813b321fcb91c7f96
Kevin Yeong <kyeong1@yahoo.com>
2019-06-25 16:49:26 +0000

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _putchar - writes to stdout
+ *
+ *
+ * Return: stdout print "Holberton" followed by newline
+ *
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+return (0);
+}
