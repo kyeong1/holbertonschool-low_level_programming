7d3e4dc76c2e594801b98328a066ebece2a2068a
Kevin Yeong <kyeong1@yahoo.com>
2019-06-25 17:47:59 +0000

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include "_putchar.c"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabetx10(void)
+{
+int a, k;
+a = 'a';
+for (k = 0; k < 10; k++)
+{
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+a = 'a';
+}
+}
