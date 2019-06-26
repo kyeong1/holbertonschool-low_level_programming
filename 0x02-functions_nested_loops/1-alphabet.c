d5d0200379becd6e4a8dcff594b8bf9d06433629
Kevin Yeong <kyeong1@yahoo.com>
2019-06-26 00:32:21 +0000

    Print lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
  * print_alphabet - uses print_alphabet function
  * @void - no argument
  * Return: Always 0.
- */
+ **/
 void print_alphabet(void)
 {
 int a;
-for (a = 'a'; a <= 'z'; a++)
+for (a == 'a'; a <= 'z'; a++)
 {
 _putchar(a);
 _putchar('\n');
