c9076d9b0e9bce8aa487f3d5d41ab2813b27d274
Kevin Yeong <kyeong1@yahoo.com>
2019-06-26 06:42:14 +0000

    Prints sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,19 +5,19 @@
  * Return : 1 if n > 0, 0 if n is zero, -1 if n is < 0
  *
  */
-int print_sign(int n)
+int print_sign(int c)
 {
-if (n > 0)
+if (c > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (n == 0)
+else if (c == 0)
 {
 _putchar ('0');
 return (0);
 }
-else (n < 0);
+else (c < 0);
 {
 _putchar ('-');
 return (-1);
