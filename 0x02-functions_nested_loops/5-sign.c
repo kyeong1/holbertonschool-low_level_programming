ea1a6c34d7c84838a75982cb099801478b5c8401
Kevin Yeong <kyeong1@yahoo.com>
2019-06-26 16:09:19 +0000

    Prints sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,16 +10,16 @@ int print_sign(int c)
 if (c > 0)
 {
 _putchar('+');
-return (1);
 }
+return (1);
 else if (c == 0)
 {
 _putchar ('0');
-return (0);
 }
+return (0);
 else (c < 0);
 {
 _putchar ('-');
-return (-1);
 }
+return (-1);
 }
