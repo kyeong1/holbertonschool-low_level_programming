c74c334562af68a11cb8abaa29e7cd7c79454dc0
Kevin Yeong <kyeong1@yahoo.com>
2019-06-28 15:29:22 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,26 @@ int main(void)
 {
 int k;
 for (k = 1; k < 100; k++)
-if (k % 15 == 0)
+if (k % 3 == 0)
 {
-printf("FizzBuzz");
+printf("Fizz");
+printf(" ");
 }
 else if (k % 5 == 0)
 {
 printf("Buzz");
+printf(" ");
 }
-else if (k % 3 == 0)
+else if (k % 15 == 0)
 {
-printf("Fizz");
+printf("FizzBuzz");
+printf(" ");
 }
 else
 {
 printf("%d", k);
+printf(" ");
 }
-printf("Buzz");
 printf("\n");
 return (0);
 }
