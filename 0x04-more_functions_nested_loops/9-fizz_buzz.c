73f1974ad58ae9a80e9690f2c3c9db30224f8191
Kevin Yeong <kyeong1@yahoo.com>
2019-06-28 04:45:48 +0000

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry
+ * @void: no int
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int k;
+for (k = 1; k < 100; k++)
+if (k % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else if ( k % 5 == 0)
+{
+printf("Buzz");
+}
+else if (k % 3 == 0)
+{
+printf("Fizz");
+}
+else
+{
+printf("%d", k);
+}
+printf("Buzz");
+printf("\n");
+return (0);
+}
