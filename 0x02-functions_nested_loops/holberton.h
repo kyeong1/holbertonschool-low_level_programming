fa697e3e41e1bd15bb71331e6d0e6ab38d04e7f2
Kevin Yeong <kyeong1@yahoo.com>
2019-06-28 05:30:25 +0000

    Header update

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,33 +1,6 @@
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c)
-
-/**
- *
- * prints lowercase alphabet followed by new line
- **/
 void print_alphabet(void);
-
-/**
- * prints lowercase alphabet 10 times
- *
- **/
 void print_alphabet_x10(void);
-
-/**
- *
- * checks for lowercase characters
- *
- **/
 int _islower(int c);
-
-/**
- * checks for alphabetic character
- *
- **/
 int _isalpha(int c);
+int add(int, int);
